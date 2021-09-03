c32f5dc7d2af5f42fdea83b6069d7be022388a5a
anitarestrepo16 <anitarestrepo16@gmail.com>
2021-09-03 12:55:42 -0500

    invent sleep items as fillers

--- social-stroop/stroop_template.js
+++ social-stroop/stroop_template.js
@@ -105,13 +105,16 @@ var sleep1 = {
     stimulus: `<p style = 'color: white;'> During the past month, how often have you had trouble sleeping because you cannot get to sleep within 30 minutes? </p>`,
     choices: ['Not during the past month', 'Less than once a week', 'Once or twice a week', 'Three or more times a week'],
     data: {
-        task: "sleep1"
+        task: "PSQI5a"
     }
 }
 var sleep2 = {
     type: "html-button-response",
     stimulus: `<p style = 'color: white;'> During the past month, how often have you had trouble sleeping because you wake up in the middle of the night or early morning? </p>`,
-    choices: ['Not during the past month', 'Less than once a week', 'Once or twice a week', 'Three or more times a week']
+    choices: ['Not during the past month', 'Less than once a week', 'Once or twice a week', 'Three or more times a week'],
+    data: {
+        task: "PSQI5b"
+    }
 }
 var sleep3 = {
     type: "html-button-response",
@@ -173,6 +176,12 @@ var sleep13 = {
 }
 
 var sleep14 = {
+    type: "html-button-response",
+    stimulus: `<p style = 'color: white;'> On avergae, how do you think your sleep quality compares to that of other people in your life (friends, family, etc.)? </p>`,
+    choices: ['A lot worse', 'Slightly worse', 'About the same', 'Slightly better', 'A lot better']
+}
+
+var sleep15 = {
     type: "survey-text",
     questions: [
         {prompt: `<p style = 'color: white;'> During the past month, when have you usually gone to bed at night? </p>`, placeholder: `Type text here`}
@@ -180,7 +189,7 @@ var sleep14 = {
     button_label: `Click to continue>`
 }
 
-var sleep15 = {
+var sleep16 = {
     type: "survey-text",
     questions: [
         {prompt: `<p style = 'color: white;'> During the past month, how long (in minutes) has it usually taken you to fall asleep each night? </p>`, placeholder: `Type text here`}
@@ -188,7 +197,7 @@ var sleep15 = {
     button_label: `Click to continue>`
 }
 
-var sleep16 = {
+var sleep17 = {
     type: "survey-text",
     questions: [
         {prompt: `<p style = 'color: white;'> During the past month, when have you usually gotten up in the morning? </p>`, placeholder: `Type text here`}
@@ -196,7 +205,7 @@ var sleep16 = {
     button_label: `Click to continue>`
 }
 
-var sleep17 = {
+var sleep18 = {
     type: "survey-text",
     questions: [
         {prompt: `<p style = 'color: white;'> During the past month, how many hours of actual sleep did you get at night? (this may be different than the number of hours you spend in bed) </p>`, placeholder: `Type text here`}
@@ -204,18 +213,18 @@ var sleep17 = {
     button_label: `Click to continue>`
 }
 
-var sleep18 = {
+var sleep19 = {
     type: "survey-text",
     questions: [
-        {prompt: `<p style = 'color: white;'> During the past month, have any external circumstances (for example crying babies, barking dogs, street noise) prevented you from getting a good night's sleep? Please explain. </p>`, placeholder: `Type text here`}
+        {prompt: `<p style = 'color: white;'> During the past month, have any external circumstances (for example crying babies, barking dogs, street noise) prevented you from getting a good night's sleep? Please explain. </p>`, placeholder: `Type text here`, name: "sleep_external"}
     ],
     button_label: `Click to continue>`
 }
 
-var sleep19 = {
+var sleep20 = {
     type: "survey-text",
     questions: [
-        {prompt: `<p style = 'color: white;'> How do you think your sleep quality compares to that of other people in your life (friends, family, etc.)? </p>`, placeholder: `Type text here`}
+        {prompt: `<p style = 'color: white;'> In the past month, has anyone in your household (apart from yourself) experienced problems sleeping? </p>`, placeholder: `Type text here`, name: 'household_sleep'}
     ],
     button_label: `Click to continue>`
 }
@@ -521,7 +530,7 @@ var random_order = jsPsych.randomization.shuffle(blocks);
 
 /* create another level of nested timelines where we intersperse the blocks of words with the sleep questionnaire items */
 var real_trials = {
-    timeline: [random_order[0], sleep14, sleep1, sleep2, sleep3, sleep_wait, random_order[1], sleep15, sleep4, sleep5, sleep6, sleep_wait, random_order[2], sleep16, sleep7, sleep8, sleep9, sleep_wait, random_order[3], sleep17, sleep10, sleep11, sleep12, sleep_wait, random_order[4],sleep18, sleep13, sleep_wait, random_order[5]]
+    timeline: [random_order[0], sleep15, sleep1, sleep2, sleep3, sleep_wait, random_order[1], sleep16, sleep4, sleep5, sleep6, sleep_wait, random_order[2], sleep17, sleep7, sleep8, sleep9, sleep_wait, random_order[3], sleep18, sleep10, sleep11, sleep12, sleep_wait, random_order[4],sleep19, sleep13, sleep15, sleep20, sleep_wait, random_order[5]]
 }
 /* add the real trials with their nested timelines to the larger first-level timeline */
 timeline.push(real_trials);
