ce9e53787856e991322f0f9c7fe4abeff3d1ff13
anitarestrepo16 <anitarestrepo16@gmail.com>
2021-09-05 18:40:53 -0500

    updated javascript to save data

--- social-stroop/template_qualtrics_javascript.js
+++ social-stroop/template_qualtrics_javascript.js
@@ -10,16 +10,18 @@ Qualtrics.SurveyEngine.addOnload(function () {
     qthis.hideNextButton();
 
     /* Change 2: Defining and load required resources */
-    var task_github = "https://anitarestrepo16.github.io/new_stroop/"; // https://<your-github-username>.github.io/<your-experiment-name>
+    var github_url = "https://ar277.github.io/new_stroop/social-stroop";
 
-    // requiredResources must include all the JS files that demo-simple-rt-task-transformed.html uses.
+    // the below urls must be accessible with your browser
+    // for example, https://kywch.github.io/jsPsych/jspsych.js
     var requiredResources = [
-        task_github + "jspsych.js",
-		task_github + "plugins/jspsych-instructions.js",
-        task_github + "plugins/jspsych-html-keyboard-response.js",
-		task_github + "plugins/jspsych-survey-text.js",
-		task_github + "plugins/jspsych-html-button-response.js",
-        task_github + "social-stroop/Baseline_pool1.js" // change this to whatever task file is needed
+        github_url + "../jspsych.js",
+        github_url + "../plugins/jspsych-html-keyboard-response.js",
+		github_url + "../plugins/jspsych-instructions.js",
+		github_url + "../plugins/jspsych-survey-text.js",
+		github_url + "../plugins/jspsych-html-button-response.js",
+		github_url + "Baseline_pool1.js", // make sure this is calling the right task
+		"https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"
     ];
 
     function loadScript(idx) {
@@ -42,32 +44,51 @@ Qualtrics.SurveyEngine.addOnload(function () {
     jQuery("<div id = 'display_stage_background'></div>").appendTo('body');
     jQuery("<div id = 'display_stage'></div>").appendTo('body');
 
+    /* Change 6: Defining necessary variables and functions for saving the results */
+    // experimental session-defining variables
+    var task_name = "Baseline_pool1"; // change this for each individual task
+    var sbj_id = "${e://Field/workerId}";
+
+    // you must put your save_data php url here.
+    var save_url = "https://users.rcc.uchicago.edu/~ar277/exp_data/save_data.php";
+    var data_dir = task_name;
+
+    // my preference is to include the task and sbj_id in the file name
+    var file_name = task_name + '_' + sbj_id; 
+
+    function save_data_csv() {
+        jQuery.ajax({
+            type: 'post',
+            cache: false,
+            url: save_url,
+            data: {
+                data_dir: data_dir,
+                file_name: file_name + '.csv', // the file type should be added
+                exp_data: jsPsych.data.get().csv()
+            }
+        });
+    }
+
     /* Change 4: Wrapping jsPsych.init() in a function */
     function initExp() {
 
         jsPsych.init({
             timeline: timeline,
             display_element: 'display_stage',
+
+            /* Change 5: Adding the clean up and continue functions.*/
             on_finish: function (data) {
-                /* Change 5: Summarizing and save the results to Qualtrics */
-                // summarize the results
-                var trials = jsPsych.data.get().filter({
-                    test_part: 'test'
-                });
-                var correct_trials = trials.filter({
-                    correct: true
-                });
-                var accuracy = Math.round(correct_trials.count() / trials.count() * 100);
-                var rt = Math.round(correct_trials.select('rt').mean());
 
-                // save to qualtrics embedded data
-                Qualtrics.SurveyEngine.setEmbeddedData("pre_accuracy", accuracy);
-                Qualtrics.SurveyEngine.setEmbeddedData("pre_rt", rt);
+                /* Change 7: Calling the save function -- CHOOSE ONE! */
+                // include the participant ID in the data
+                // this must be done before saving
+                jsPsych.data.get().addToLast({participant: sbj_id});
 
-                /* Change 6: Adding the clean up and continue functions.*/
+                save_data_csv();
+
                 // clear the stage
-                jQuery('#display_stage').remove();
-                jQuery('#display_stage_background').remove();
+                jQuery('display_stage').remove();
+                jQuery('display_stage_background').remove();
 
                 // simulate click on Qualtrics "next" button, making use of the Qualtrics JS API
                 qthis.clickNextButton();
